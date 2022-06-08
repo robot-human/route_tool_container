ef352fbc8df15d111151fd7ce797d68ffc3ba53c
prodriguez <rodriguez_p@fev.com>
2022-06-08 17:11:42 -0500

    jun 8

--- server_publish.py
+++ server_publish.py
@@ -42,7 +42,6 @@ def on_subscribe(client, userdata, mid, granted_qos, properties=None):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 def on_publish(client, userdata, mid, properties=None):
     print("mid: " + str(mid))
-    time.sleep(KEEPALIVE)
 
 
 if __name__ == '__main__':
@@ -66,11 +65,13 @@ if __name__ == '__main__':
     for name in output_files:
         file_name_path = output_files_path+name
         client.publish(topic, payload=f"{name}", qos=QOS)
+        time.sleep(KEEPALIVE)
         print(name)
         f = open(file_name_path, "r")
         content = f.read()
         print(getsizeof(content)/1000, " kbts")
         client.publish(topic, payload=content, qos=QOS)
+        time.sleep(KEEPALIVE)
         f.close()
         print(f"{name} closed")
     
