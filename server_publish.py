174468a69f6f8db4323babaf18b89076a6595f4b
prodriguez <rodriguez_p@fev.com>
2022-06-09 08:24:15 -0500

    jun 8

--- server_publish.py
+++ server_publish.py
@@ -45,7 +45,6 @@ def on_publish(client, userdata, mid, properties=None):
 
 
 if __name__ == '__main__':
-    count = 0
     print("start sending files")
     client = paho.Client(client_id=clientID, userdata=None, protocol=paho.MQTTv5)
     client.on_connect = on_connect
@@ -67,21 +66,15 @@ if __name__ == '__main__':
         file_name_path = output_files_path+name
         #client.publish(topic, payload=f"{name}", qos=QOS)
         publish.single(topic, payload=f"{name}", qos=QOS, retain=False, hostname=host,port=port, client_id=clientID, keepalive=KEEPALIVE, will=None, auth=None, tls=None,protocol=paho.MQTTv5, transport="tcp")
-        #time.sleep(1)
         print(name)
         f = open(file_name_path, "r")
         content = f.read()
         print(getsizeof(content)/1000, " kbts")
         #client.publish(topic, payload=content, qos=QOS)
         publish.single(topic, payload=content, qos=QOS, retain=False, hostname=host,port=port, client_id=clientID, keepalive=KEEPALIVE, will=None, auth=None, tls=None,protocol=paho.MQTTv5, transport="tcp")
-        #time.sleep(1)
         f.close()
-        print(f"{name} closed")
-        if(count < n_files+1):
-            count += 1        
+        print(f"{name} closed")     
     
-    #client.loop_forever()
     client.on_disconnect = on_disconnect
     client.disconnect()
-    #client.loop_forever()
     
