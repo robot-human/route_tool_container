a2c955376acacd6679769401b18424a33a68f685
prodriguez <rodriguez_p@fev.com>
2022-07-11 08:31:50 -0500

    highway ready

--- server_subscribe.py
+++ server_subscribe.py
@@ -3,8 +3,8 @@ import paho.mqtt.client as paho
 from paho import mqtt
 
 SERVER = 0
-topic = "fevvf/route_tool_dylan"
-clientID = "clientId-xLODDl314VwO-dylan-s"
+topic = "fevvf/route_tool_karthik"
+clientID = "clientId-xLODDl314VwO-karthik-s"
 file_path = f"./request_input.txt"
 
 if(SERVER == 0):
