2c86c7d050b9754ff933fb4948dd9f29c3262146
prodriguez <rodriguez_p@fev.com>
2022-07-11 07:36:55 -0500

    highway ready

--- server_publish.py
+++ server_publish.py
@@ -7,8 +7,8 @@ import paho.mqtt.publish as publish
 from time import sleep
 
 SERVER = 0
-topic = "fevvf/route_tool_dylan"
-clientID = "clientId-xMODDl314VwR-dylan-p"
+topic = "fevvf/route_tool_prakash"
+clientID = "clientId-xMODDl314VwR-prakash-p"
 file_path = f'./config.ini'
 QOS = 2
 KEEPALIVE=60
