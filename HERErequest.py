32be7352bc988125d2983bfdd5242a2d7a87622d
prodriguez <rodriguez_p@fev.com>
2022-07-13 13:48:37 -0500

    highway ready

--- HERErequest.py
+++ HERErequest.py
@@ -553,7 +553,7 @@ def requestRoadGeomTile(links_dict: dict,  tile: tuple, cfg: dict, session: requ
                             links_dict[link_id]['HIGHWAY'] = 'Y'
                         elif(links_dict[link_id]['FUNCTIONAL_CLASS'] in [3,4,5]):
                             links_dict[link_id]['HIGHWAY'] = 'N'
-                            if((links_dict[link_id]['SPEED_CATEGORY'] in [1,2,3,4]) and (links_dict[link_id]['TRAVEL_DIRECTION'] == 'B') and (links_dict[link_id]['LANE_CATEGORY'] == 1) and (links_dict[link_id]['URBAN'] != 'Y') and (links_dict[link_id]['LOW_MOBILITY'] in [1,3])):
+                            if((links_dict[link_id]['SPEED_CATEGORY'] in [1,2,3,4]) and (links_dict[link_id]['TRAVEL_DIRECTION'] == 'B') and (links_dict[link_id]['LANE_CATEGORY'] == 1) and (links_dict[link_id]['URBAN'] != 'Y') and (links_dict[link_id]['LOW_MOBILITY'] == 1)):
                                 links_dict[link_id]['CITY'] = 'N'
                             else:
                                 links_dict[link_id]['CITY'] = 'Y'
